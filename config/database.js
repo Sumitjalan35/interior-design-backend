6c96a6a454b221268dccead4e5967a9f8b26a631
Sumitjalan35 <sumitjalan3534@gmail.com>
2025-07-17 22:54:05 +0530

    database.js

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/interior-design', {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/interior-design', {
       // Removed deprecated options: useNewUrlParser and useUnifiedTopology
     });
 
