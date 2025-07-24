91fe8089e3306511eabdda1e94a719d25bc9a7c0
Sumitjalan35 <sumitjalan3534@gmail.com>
2025-07-24 22:39:41 +0530

    Update upload.js

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,1 +1,57 @@
+const multer = require('multer');
+const { v2: cloudinary } = require('cloudinary');
+const fs = require('fs');
+const { ApiError } = require('./apierror');
 
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public');
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({ storage });
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+});
+
+const uploadfile = async (localfilepath) => {
+  try {
+    if (!localfilepath) return null;
+    const responsefromcloud = await cloudinary.uploader.upload(localfilepath, {
+      resource_type: 'auto',
+    });
+    console.log(responsefromcloud);
+    fs.unlinkSync(localfilepath);
+    return responsefromcloud;
+  } catch (err) {
+    console.log(err);
+    fs.unlinkSync(localfilepath);
+    return null;
+  }
+};
+
+const deletefile = async (publicId) => {
+  try {
+    if (!publicId) {
+      throw new ApiError(400, 'the file path is missing');
+    }
+    const response = await cloudinary.uploader.destroy(publicId);
+    console.log(response);
+    return response;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+module.exports = {
+  upload,
+  uploadfile,
+  deletefile
+}; 
